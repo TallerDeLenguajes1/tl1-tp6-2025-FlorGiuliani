d24010429654c469a7c0c2bbc323a49d138bf7c2
Flor Giuliani <florgiuliani5@gmail.com>
2025-05-20 16:36:13 -0300

    Realice el Ejercicio 1

---
+++ Proyecto1/Program.cs
@@ -0,0 +1,29 @@
+﻿// See https://aka.ms/new-console-template for more information
+/*Console.WriteLine("Hello, World!");
+
+Console.WriteLine("Hello, World!");
+int a;
+int b;
+a=10;
+b=a;
+Console.WriteLine("valor de a:"+a);
+Console.WriteLine("valor de b:"+b);*/
+
+Console.WriteLine("Ingrese un numero: ");
+string? entrada = Console.ReadLine();
+int numero;
+double inverso = 0;
+
+if (int.TryParse(entrada, out numero))
+{
+    int cifras = entrada.Length;
+    while (numero > 0)
+    {
+        inverso += numero % 10 * Math.Pow(10, cifras - 1);
+        numero /= 10;
+        cifras--;
+    }
+    Console.WriteLine("El inverso del numero ingresado es: " + inverso);
+} else {
+    Console.WriteLine("No ingresaste ningun numero");
+}
